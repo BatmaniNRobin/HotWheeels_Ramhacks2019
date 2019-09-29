d71565a367c7c7b817c67f53c89ec990a20e1ffd
Mohammad Malik <malikmui@vcu.edu>
2019-09-29 07:28:16 -0400

    yessir

--- hot_wheels.py
+++ hot_wheels.py
@@ -10,6 +10,181 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+data = {
+    'make':"Toyota",
+    'model':"Altered Ego",
+    'year':"2020",
+    'location'
+    :"(1,2)",
+    'reserved'
+    :'true'
+,
+    'make'
+    :"Ford",
+    'model'
+    :"Rigor Motor",
+    'year'
+    :"2019",
+    'location'
+    :"(1,4)",
+    'reserved'
+    :'true'
+,
+    'make'
+    :"Chevrolet",
+    'model'
+    :"Torque Screw",
+    'year'
+    :"2018",
+    'location'
+    :"(1,5)",
+    'reserved'
+    :'false'
+,
+    'make',
+    :"Toyota"
+    'model'
+    :"Altered Ego",
+    'year'
+    :"2020",
+    'location'
+    :"(1,1)",
+    'reserved'
+    :'false'
+,
+    'make'
+    :"Ford",
+    'model'
+    :"Rigor Motor",
+    'year'
+    :"2019",
+    'location'
+    :"(1,3)",
+    'reserved'
+    :'false'
+,
+    'make'
+    :"Chevrolet",
+    'model'
+    :"Torque Screw",
+    'year'
+    :"2018",
+    'location'
+    :"(1,6)",
+    'reserved'
+    :'false'
+,
+    'make'
+    :"Chevrolet",
+    'model'
+    :"Torque Screw",
+    'year'
+    :"2018",
+    'location'
+    :"(2,1)",
+    'reserved'
+    :'false'
+,
+    make
+    :"Bugatti"
+    model
+    :"Covelight"
+    year
+    :"2017"
+    location
+    :"(2,2)"
+    reserved
+    :true
+,
+    make
+    :"Bugatti"
+    model
+    :"Covelight"
+    year
+    :"2017"
+    location
+    :"(2,3)"
+    reserved
+    :true
+,
+    make
+    :"Bugatti"
+    model
+    :"Covelight"
+    year
+    :"2017"
+    location
+    :"(2,4)"
+    reserved
+    :false
+,
+    make
+    :"Lamborghini"
+    model
+    :"Power Rocket"
+    year
+    :"2016"
+    location
+    :"(2,5)"
+    reserved
+    :false
+,
+    make
+    :"Lamborghini"
+    model
+    :"Power Rocket"
+    year
+    :"2016"
+    location
+    :"(2,6)"
+    reserved
+    :true
+,
+    make
+    :"Lamborghini"
+    model
+    :"Power Rocket"
+    year
+    :"2016"
+    location
+    :"(3,1)"
+    reserved
+    :false
+,
+    make
+    :"Honda"
+    model
+    :"Rocket Box"
+    year
+    :"2015"
+    location
+    :"(3,2)"
+    reserved
+    :false
+,
+    make
+    :"Honda"
+    model
+    :"Rocket Box"
+    year
+    :"2015"
+    row
+    :"(3,3)"
+    reserved
+    :false
+,
+    make
+    :"Honda"
+    model
+    :"Rocket Box"
+    year
+    :"2015"
+    location
+    :"(3,4)"
+    reserved
+    :false
+}
+
 client = pymongo.MongoClient('mongodb+srv://admin:ramHacks2019@hotwheels-gmjn8.gcp.mongodb.net/test')
 db = client.Whips
 collection = db.HotWheels
