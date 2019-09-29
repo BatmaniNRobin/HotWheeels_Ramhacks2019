5a824d884c0c1875def555bfdf1816d3f99c7105
Mohammad Malik <32021980+BatmaniNRobin@users.noreply.github.com>
2019-09-29 01:01:24 -0400

    upd

--- hot_flask.py
+++ hot_flask.py
@@ -1,5 +1,21 @@
+
+import json
 from flask import Flask
+
+import pymongo
+import dns
+from bson.json_util import dumps
+
 app = Flask(__name__)
+
+client = pymongo.MongoClient('mongodb+srv://hotwheels-gmjn8.gcp.mongodb.net/test')
+# password = "ramHacks2019"
+
+db = client.Whips
+
+collection = db['HotWheels']
+
+# a = db.collection.find({}, {_id:1}.map(function(item):return item._id; })
 
 @app.route('/')
 def index():
@@ -8,3 +24,13 @@ def index():
 @app.route('/great')
 def say_hello():
     return 'Hello from server'
+# make endpoints
+
+@app.route('/cars')
+def cars():
+    return { "cars": db._id_ }
+
+@app.route('/car/<id>')
+def get_car(id):
+    return {}
+
